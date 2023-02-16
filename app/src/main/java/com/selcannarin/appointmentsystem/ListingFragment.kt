4c5b6534659f1a9f6a3e07274f63f15992231d21
Selcan Narin <selcannarin12@gmail.com>
2023-02-16 22:01:01 +0300

    ListingFragment

--- app/src/main/java/com/selcannarin/appointmentsystem/ListingFragment.kt
+++ app/src/main/java/com/selcannarin/appointmentsystem/ListingFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class ListingScreenFragment : Fragment() {
+class ListingFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +17,8 @@ class ListingScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listing_screen, container, false)
+        return inflater.inflate(R.layout.fragment_listing, container, false)
     }
+
 
 }
